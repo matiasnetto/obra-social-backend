b61aa1e428534a229cb1fcdcbca32192d4ed15fe
MatiasNetto <matiasnetto03@gmail.com>
2022-06-04 12:54:55 -0300

    Fix deploy bug

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ dotenv.config();
 import './config/mongo';
 import app from './config/server';
 
-app.listen(app.get('PORT'), '192.168.1.62', () => {
+app.listen(app.get('PORT'), () => {
   console.log('Server listening on  port' + app.get('PORT'));
 });
